a0be97f653942c292eaa1862422745ed552f99a0
stevealbertwong <wongkwunkit@gmail.com>
2018-04-11 14:14:31 +0800

    p181 creates odometry

---
+++ build/robot1_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/steve/dev/catkin_ws/src/robot1_description
+# Build directory: /home/steve/dev/catkin_ws/build/robot1_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
